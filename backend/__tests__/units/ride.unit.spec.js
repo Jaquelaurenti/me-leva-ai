7407b0417aa6940c3675205401281b5a82d71e3f
Jaque Laurenti <jaquelinelaurenti@gmail.com>
2022-07-13 18:44:26 -0300

    add mocks

--- backend/__tests__/units/ride.unit.spec.js
+++ backend/__tests__/units/ride.unit.spec.js
@@ -1,6 +1,12 @@
 const rideService = require('../../frota-veiculo-apis/src/services/RideService');
 const mockingoose = require('mockingoose');
-
+const vehicleBusyMock  = require('./mocks/vehicleBusyMock.json');
+const userMock  = require('./mocks/userMock.json');
+const newRideMock  = require('./mocks/newRideMock.json');
+const vehicleAvailableMock  = require('./mocks/vehicleAvailableMock.json');
+const rideMock  = require('./mocks/rideMock.json');
+const rideStartedMock = require('./mocks/rideStartedMock.json');
+const rideFineshedMock = require('./mocks/rideFineshedMock.json');
 const rideModel = require("../../frota-veiculo-apis/src/models/Rides");
 const vehicleModel = require("../../frota-veiculo-apis/src/models/Vehicles");
 const userModel = require("../../frota-veiculo-apis/src/models/Users");
@@ -11,166 +17,51 @@ afterEach(() => {
 
 describe('Teste de Corrida', () => {
   test('Deverá inserir uma corrida', async () => {
-    const data = {
-      user: '0123456789',
-      vehicle: 'xxx-0000',
-      startPlace: 'Teste',
-      finishPlace: 'Teste Final',
-      status: 'asked'
-    }
-
-
-    const userData = {
-      _id: "62b8df3b85371c1b1502e791",
-      name: 'teste',
-      email: 'teste@teste.com',
-      password: '123456',
-      telephone: '0123456789'
-    };
-
-    const vehicleData = {
-      _id: "62bf83f8818b2e4b5795fe0c",
-      licensePlate: "xxx-0000",
-      model: "Tesla",
-      status: "avaialable",
-      createdAt: "2022-07-01T23:32:08.976Z",
-    };
 
     //Mockando o usuario
-    mockingoose(userModel).toReturn(userData, 'findOne');
+    mockingoose(userModel).toReturn(userMock, 'findOne');
 
     //Mockando o veículo
-    mockingoose(vehicleModel).toReturn(vehicleData, 'findOne');
+    mockingoose(vehicleModel).toReturn(vehicleAvailableMock, 'findOne');
 
-    const vehicleDataUpdate = {
-      _id: "62bf83f8818b2e4b5795fe0c",
-      licensePlate: "xxx-0000",
-      model: "Tesla",
-      status: "busy",
-      createdAt: "2022-07-01T23:32:08.976Z",
-    };
     //Mockando o veículo para o satus busy
-    mockingoose(vehicleModel).toReturn(vehicleDataUpdate, 'findOneAndUpdate');
+    mockingoose(vehicleModel).toReturn(vehicleBusyMock, 'findOneAndUpdate');
 
-    const response = await rideService.ask(data.user, data.startPlace, data.finishPlace);
+    const response = await rideService.ask(newRideMock.user, newRideMock.startPlace, newRideMock.finishPlace);
     expect(response.statusCode).toBe(201);
     expect(response.data.status).toBe('asked');
     expect(response.data.startPlace).toBe('Teste');
     expect(response.data.finishPlace).toBe('Teste Final');
-    expect(JSON.parse(JSON.stringify(response.data.user))).toMatchObject(userData);
-    expect(JSON.parse(JSON.stringify(response.data.vehicle))).toMatchObject(vehicleDataUpdate);
+    expect(response.data.status).toBe('asked');
+    expect(JSON.parse(JSON.stringify(response.data.user))).toMatchObject(userMock);
+    expect(JSON.parse(JSON.stringify(response.data.vehicle))).toMatchObject(vehicleBusyMock);
   });
 
   test('Deverá iniciar uma corrida', async () => {
     const status = "start";
-    const rideData = {
-      _id: '62c49e69b52490951c26dd70',
-      user: {
-        _id: '62b8df3b85371c1b1502e791',
-        name: 'teste',
-        email: 'teste@teste.com',
-        password: '123456',
-        telephone: '0123456789',
-        createdAt: '2022-07-05T20:26:17.420Z'
-      },
-      vehicle: {
-        _id: '62bf83f8818b2e4b5795fe0c',
-        licensePlate: 'xxx-0000',
-        model: 'Tesla',
-        status: 'busy',
-        createdAt: '2022-07-01T23:32:08.976Z'
-      },
-      startPlace: 'Teste',
-      finishPlace: 'Teste Final',
-      status: 'asked',
-      createdAt: '2022-07-05T20:26:17.430Z'
-    }
-
     //Mockando o getRides
-    mockingoose(rideModel).toReturn(rideData, 'findOne');
+    mockingoose(rideModel).toReturn(rideMock, 'findOne');
     //mockando o updateStatus
-    const rideDataUpdate = {
-      _id: '62c49e69b52490951c26dd70',
-      user: {
-        _id: '62b8df3b85371c1b1502e791',
-        name: 'Teste',
-        email: 'teste@mail.com',
-        telephone: '99999999',
-        password: '123456',
-        createdAt: '2022-07-05T20:26:17.420Z'
-      },
-      vehicle: {
-        _id: '62bf83f8818b2e4b5795fe0c',
-        licensePlate: 'xxx-0000',
-        model: 'Tesla',
-        status: 'busy',
-        createdAt: '2022-07-01T23:32:08.976Z'
-      },
-      startPlace: 'Teste',
-      finishPlace: 'Teste Final',
-      status: 'started',
-      createdAt: '2022-07-05T20:26:17.430Z'
-    }
-    mockingoose(rideModel).toReturn(rideDataUpdate, 'findOneAndUpdate');
 
-    const response = await rideService.updateStatus(rideData._id, status);
-    expect(JSON.parse(JSON.stringify(response.data))).toMatchObject(rideDataUpdate);
+    mockingoose(rideModel).toReturn(rideStartedMock, 'findOneAndUpdate');
+
+    const response = await rideService.updateStatus(rideMock._id, status);
+    expect(response.data.status).toBe('started');
+    expect(JSON.parse(JSON.stringify(response.data))).toMatchObject(rideStartedMock);
   });
 
   test('Deverá finalizar uma corrida', async () => {
     const status = "finish";
-    const rideData = {
-      _id: '62c49e69b52490951c26dd70',
-      user: {
-        _id: '62b8df3b85371c1b1502e791',
-        name: 'teste',
-        email: 'teste@teste.com',
-        password: '123456',
-        telephone: '0123456789',
-        createdAt: '2022-07-05T20:26:17.420Z'
-      },
-      vehicle: {
-        _id: '62bf83f8818b2e4b5795fe0c',
-        licensePlate: 'xxx-0000',
-        model: 'Tesla',
-        status: 'busy',
-        createdAt: '2022-07-01T23:32:08.976Z'
-      },
-      startPlace: 'Teste',
-      finishPlace: 'Teste Final',
-      status: 'started',
-      createdAt: '2022-07-05T20:26:17.430Z'
-    }
 
     //Mockando o getRides
-    mockingoose(rideModel).toReturn(rideData, 'findOne');
+    mockingoose(rideModel).toReturn(rideStartedMock, 'findOne');
     //mockando o updateStatus
-    const rideDataUpdate = {
-      _id: '62c49e69b52490951c26dd70',
-      user: {
-        _id: '62b8df3b85371c1b1502e791',
-        name: 'Teste',
-        email: 'teste@mail.com',
-        telephone: '99999999',
-        password: '123456',
-        createdAt: '2022-07-05T20:26:17.420Z'
-      },
-      vehicle: {
-        _id: '62bf83f8818b2e4b5795fe0c',
-        licensePlate: 'xxx-0000',
-        model: 'Tesla',
-        status: 'busy',
-        createdAt: '2022-07-01T23:32:08.976Z'
-      },
-      startPlace: 'Teste',
-      finishPlace: 'Teste Final',
-      status: 'finished',
-      createdAt: '2022-07-05T20:26:17.430Z'
-    }
-    mockingoose(rideModel).toReturn(rideDataUpdate, 'findOneAndUpdate');
 
-    const response = await rideService.updateStatus(rideData._id, status);
-    expect(JSON.parse(JSON.stringify(response.data))).toMatchObject(rideDataUpdate);
+    mockingoose(rideModel).toReturn(rideFineshedMock, 'findOneAndUpdate');
+
+    const response = await rideService.updateStatus(rideStartedMock._id, status);
+    expect(response.data.status).toBe('finished');
+    expect(JSON.parse(JSON.stringify(response.data))).toMatchObject(rideFineshedMock);
   });
 });
 
